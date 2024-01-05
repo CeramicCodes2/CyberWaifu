383c03759d36410ae1124878218d714fff15e98a
CeramicCodes2 <ivansanpan@gmail.com>
2024-01-05 05:29:28 -0600

    stashckanges

---
+++ Server/BetaLive/indx/main.js
@@ -0,0 +1,103 @@
+// ALIAS DEFINITION
+const Live2DModel = PIXI.live2d.Live2DModel,
+Application = PIXI.Application,
+Sprite = PIXI.Sprite.from,
+live2d = PIXI.live2d;
+
+class Waifu{
+    'use strict'
+    constructor(waifuPath,app){
+        this.waifuPath = waifuPath;
+        //this.windowSize = {width:1024,height:2048};
+        this.app = app;//this.createApp();
+        this.syncLoadModel();
+    }
+    appendToLogInfo(text){
+        document.getElementById("log").innerHTML += `${text}\n`; 
+    }
+    post(){
+        // este metodo se encargara de si se cargo el modelo aplicar transformaciones y informacion de logeo ademas
+        // de desplegarlo
+        this.appendToLogInfo(this.model);
+
+        this.app.stage.addChild(this.model);
+        this.setModel();
+        this.draggable();
+        console.log(this.model.hitAreas);
+        //this.addHitAreas();
+        this.onInteraction();
+
+    }
+    syncLoadModel(){
+        /// cargar modelo de forma sincrona
+        this.model = Live2DModel.fromSync(this.waifuPath,{ onError: console.warn });
+        this.model.once('ready', ()=>this.post());
+        // cargamos el modelo
+
+        //Live2DModel.from(this.waifuPath).then((value)=>{this.model = value},(rejected)=>{this.model = undefined; this.appendToLogInfo(`Error loading the model \n :C sorry ... \n ${rejected}`)})
+        //setTimeout(()=>{Live2DModel.from(this.waifuPath).then((value)=>{this.model = value;this.post();},(rejected)=>{this.appendToLogInfo(`Error loading the model \n :C sorry ... \n ${rejected}`)})},time);
+    }
+    setModel(){
+        const scaleX = (innerWidth * 0.4) / this.model.width;
+        const scaleY = (innerHeight * 0.8) / this.model.height;
+    
+        // fit the window
+        this.model.scale.set(Math.min(scaleX, scaleY));    
+        this.model.rotation = Math.PI;
+        this.model.skew.x = Math.PI;
+        //this.model.x = 2048;
+        //this.model.y = 2048;
+        //this.model.scale.set(2, 2);
+        this.model.anchor.set(0.5, 0.5);
+    }
+    draggable() {
+        this.model.buttonMode = true;
+        this.model.on("pointerdown", (e) => {
+          this.model.dragging = true;
+          this.model._pointerX = e.data.global.x - this.model.x;
+          this.model._pointerY = e.data.global.y - this.model.y;
+        });
+        this.model.on("pointermove", (e) => {
+          if (this.model.dragging) {
+            this.model.position.x = e.data.global.x - this.model._pointerX;
+            this.model.position.y = e.data.global.y - this.model._pointerY;
+          }
+        });
+        this.model.on("pointerupoutside", () => (this.model.dragging = false));
+        this.model.on("pointerup", () => (this.model.dragging = false));
+      }
+    onClickArea(hitAreaName='body',executeMotion='TapBody'){
+        this.appendToLogInfo(this.model.hitAreas);
+
+        this.model.on('hit', (hitAreas) => {
+            if (hitAreas.includes(hitAreaName)) {
+                console.log(`hit ${hitAreas} ${executeMotion} ${JSON.stringify(this.model.motion(executeMotion))}`)
+                this.motion(executeMotion);
+                //this.model.expression(0);
+              
+            }
+          });
+    }
+    onInteraction(){
+        this.onClickArea('Body','TapBody');
+        this.onClickArea('Head','Head');
+        //this.addHitAreas();
+    }
+}
+
+function createApp(canvasId){
+    return new Application({
+        view: document.getElementById(canvasId),
+        autoStart: true,
+        //width:2048,height:2048,
+        resizeTo: window,
+        backgroundColor: 0x333333
+      });// create the pixi app
+}
+
+function main(){
+    const app = createApp('canvas');
+    let instance = new Waifu('./adult/adult.model.json',app);
+    window.PIXI = PIXI;
+    //console.log(typeof instance.model.motion)
+}
---
+++ Server/BetaLive/indx/test.js
@@ -0,0 +1,94 @@
+
+const live2d = PIXI.live2d;
+
+async function main() {
+  const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    resizeTo: window,
+    backgroundColor: 0x333333
+  });
+  let cubism2Model = './Mao/Mao.model3.json';
+  const models = await Promise.all([
+    live2d.Live2DModel.from(cubism2Model)
+  ]);
+
+  models.forEach((model) => {
+    app.stage.addChild(model);
+
+    const scaleX = (innerWidth * 0.4) / model.width;
+    const scaleY = (innerHeight * 0.8) / model.height;
+
+    // fit the window
+    model.scale.set(Math.min(scaleX, scaleY));
+
+    model.y = innerHeight * 0.1;
+
+    draggable(model);
+    addFrame(model);
+    addHitAreaFrames(model);
+  });
+
+  const model2 = models[0];
+
+  model2.x = (innerWidth - model2.width - model4.width) / 2;
+
+  // handle tapping
+
+};
+
+function draggable(model) {
+  model.buttonMode = true;
+  model.on("pointerdown", (e) => {
+    model.dragging = true;
+    model._pointerX = e.data.global.x - model.x;
+    model._pointerY = e.data.global.y - model.y;
+  });
+  model.on("pointermove", (e) => {
+    if (model.dragging) {
+      model.position.x = e.data.global.x - model._pointerX;
+      model.position.y = e.data.global.y - model._pointerY;
+    }
+  });
+  model.on("pointerupoutside", () => (model.dragging = false));
+  model.on("pointerup", () => (model.dragging = false));
+}
+
+function addFrame(model) {
+  const foreground = PIXI.Sprite.from(PIXI.Texture.WHITE);
+  foreground.width = model.internalModel.width;
+  foreground.height = model.internalModel.height;
+  foreground.alpha = 0.2;
+
+  model.addChild(foreground);
+  foreground.visible = true
+  //checkbox("Model Frames", (checked) => (foreground.visible = true));
+}
+
+function addHitAreaFrames(model) {
+  const hitAreaFrames = new live2d.HitAreaFrames();
+
+  model.addChild(hitAreaFrames);
+  hitAreaFrames.visible = true;
+  //checkbox("Hit Area Frames", (checked) => (hitAreaFrames.visible = true));
+}
+
+function checkbox(name, onChange) {
+  const id = name.replace(/\W/g, "").toLowerCase();
+
+  let checkbox = document.getElementById(id);
+
+  if (!checkbox) {
+    const p = document.createElement("p");
+    p.innerHTML = `<input type="checkbox" id="${id}"> <label for="${id}">${name}</label>`;
+
+    document.getElementById("control").appendChild(p);
+    checkbox = p.firstChild;
+  }
+
+  checkbox.addEventListener("change", () => {
+    onChange(checkbox.checked);
+  });
+
+  onChange(checkbox.checked);
+}
