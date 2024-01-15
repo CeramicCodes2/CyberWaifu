4e7a400d38872064a663173b2b53e0926616db1b
CeramicCodes2 <ivansanpan@gmail.com>
2024-01-15 03:26:02 -0600

    stage

--- Server/BetaLive/indx/ui_game.js
+++ Server/BetaLive/indx/ui_game.js
@@ -5,10 +5,148 @@ const loader = PIXI.Loader.shared,
 resources = PIXI.Loader.shared.resources,
 Sprite = PIXI.Sprite,
 TextureCache = PIXI.utils.TextureCache,
-Container = PIXI.Container;
+Container = PIXI.Container,
+Text = PIXI.Text;
+
+
+class TextWindow{
+    "use strict"
+    static instance
+    constructor(sprite,ContainerWindow,jsonLevel,value,assets){
+        if(typeof TextWindow.instance === "Object"){
+            return TextWindow.instance;
+        }
+        // singleton
+        this.ContainerWindow = ContainerWindow;
+        this.jsonLevel = jsonLevel;
+        this.assets = assets;
+        this.value = value;
+        this.multiplyPosition = value.MessageYMultiply;
+        this._next_position = value.messagePositions;
+        this._messagesObject = {};
+        this._messageHistory = {};
+        this._message_id = 1;
+        this.getWebFont();
+        this.sprite = sprite
+        this.CounterWindowObjects = 0;
+        TextWindow.instance = this;
+
+
+    }
+    updateMessageRegister(header,text){
+        /* crea una  y coloca una nueva entrada en messageHistorynueva ademas actualiza el id de mensaje  */
+        this._messageHistory[this._message_id] = {"id":this._message_id,"text":text,"header":header};
+        this._message_id++;
 
 
 
+    }
+    getWebFont(){
+        window.WebFontConfig = {
+            google: {
+                families: this.value.fontFamilies,
+            },
+        };
+        (function() {
+            const wf = document.createElement('script');
+            wf.src = `${document.location.protocol === 'https:' ? 'https' : 'http'
+            }://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js`;
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            const s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        }());
+        //https://pixijs.com/examples/text/web-font
+    }
+    getNextPosition(){
+        this._next_position = this._next_position[0] * this.multiplyPosition;
+        return this._next_position;
+    }
+    addNewMessage(header,text){
+        this.updateMessageRegister(text,header);
+        this.renderText();
+
+    }
+    createNewMessage(header,text){
+        const ContainerMessage = new Container();// header container for the message
+        const spriteTextArea = new Sprite(this.assets[this.value.assetHeader]);
+        const spriteHeaderMessage = new Sprite(this.assets[this.value.assetMessage]);
+        // sets the container size
+        ContainerMessage.width = this.value.containerMessage.w;
+        ContainerMessage.height = this.value.containerMessage.h;
+        spriteHeaderMessage.position.set(this.value.initialMessagePos.x,this.value.initialMessagePos.y);
+        // spriteHeaderMessage.width = ContainerMessage.width * 0.5;
+
+        spriteTextArea.position.set(this.value.initialMessagePos.x,this.value.initialMessagePos.y + this.value.separationBetweenHeader);// separacion
+        
+        const textHeader = new Text(header,this.value.style);
+        const textContained = new Text(text,this.value.style);
+        const resolveMessagePosition = (sprite,positionConfig)=>{
+            switch(positionConfig){
+                case 'center':
+                    return sprite.width * 0.5
+                    break;
+                case 'start':
+                    return 0
+                    break;
+                case 'end':
+                    return sprite.width * 0.8
+                    break;
+
+                default:
+                    throw new Error("Error unikown position");
+            }
+        }
+        textHeader.position.set(resolveMessagePosition(spriteHeaderMessage,this.value.headerMessagesPosition),0);
+        textContained.position.set(resolveMessagePosition(spriteHeaderMessage,this.value.messagesPosition),0);
+
+        spriteHeaderMessage.addChild(textHeader);
+        spriteTextArea.addChild(textContained);
+
+        ContainerMessage.addChild(spriteHeaderMessage);
+        ContainerMessage.addChild(spriteTextArea);
+        let npos = this.getNextPosition();
+        ContainerMessage.position.set(npos[0],npos[1]);
+        // ubicamos en x y y 
+        //this.updateMessageRegister(text,header);
+        this._messagesObject['textWindow'] = {"container":ContainerMessage,"textHeader":textHeader,'text':textContained};
+        this.CounterWindowObjects ++;
+        this.sprite.addChild(ContainerMessage);
+    }
+    destroyMessages(){
+        /* este metodo se encargara de destuir mensajes que ya no se muestran en pantalla */
+
+    }
+    renderText(){
+        if(this.CounterWindowObjects >= this.value.maxMessagesOnScreen){
+            // obtenemos la ventana mas reciente y ahi colocaremos el nuevo texto
+            // generaremos un efecto de recorrido
+            //let old_message = ['',''];
+            alert(true);
+            let counter_messages = this._message_id - 1;// el mensaje mas reciente
+            this._messagesObject.forEach((container)=>{
+                // recorreremos mensajes
+                //old_message = [container.textHeader.text,container.textContained.text];
+
+                container.textHeader.text = this._messageHistory[counter_messages].header;
+                container.textContained.text = this._messageHistory[counter_messages].text;
+
+                counter_messages--;// recorremos al mensaje de mas arriba
+                // esto actualizara hara un efecto de pila 
+
+            })
+            //Object.keys(this._messagesObject).reverse()[0];
+
+
+        }else{
+            // posible error en this._message_id por post incremento (solucion restar uno)
+            this.createNewMessage(this._messageHistory[this._message_id -1].text,this._messageHistory[this._message_id-1].header);
+        }
+    }
+    
+}
+
+//TextWindow.getWebFont(); // inicializamos y obtenemos la familia que se usara para texto
 
 class UI_GAME{
     constructor(app){
@@ -31,6 +169,8 @@ class UI_GAME{
         await this.mapButtons();
         await this.mapBar();
         await this.mapSignalAnimation();
+        await this.mapTextWindow();
+        this._spriteDict.textWindow['windowObject'].addNewMessage('IA','HELLO WORLD !')
         //this._spriteDict["love_level"]["methadata"]["alter_state"]();
 
         /*
@@ -202,21 +342,30 @@ class UI_GAME{
 
         }
     }
-    async mapTextArea(){
-        /* a text area to display the thinks what the model says or the user input */
+    async mapTextWindow(){
+        const spriteWindow = new Sprite(this.assets[this.jsonLevel.textWindow.assetWindow]);
+        const ContainerWindow = new Container();
+        spriteWindow.width = this.jsonLevel.textWindow.w;
+        spriteWindow.height = this.jsonLevel.textWindow.h;
+        spriteWindow.position.set(this.jsonLevel.textWindow.x,this.jsonLevel.textWindow.y);
+        const textureWindow = new TextWindow(spriteWindow,ContainerWindow,this.jsonLevel,this.jsonLevel.textWindow,this.assets);
+        
         await this.objectInteraction();
-        for(const [key,value] of Object.entries(this.jsonLevel.text)){
-            const ContainerWindow = new Container();
-            const ContainerMessage = new Container();// header container for the message
-            const spriteTextArea = new Sprite(this.assets[value.assetHeader]);
-            const spriteHeaderMessage = new Sprite(this.assets[value.assetMessage]);
-            const spriteWindow = new Sprite(this.assets[value.assetWindow]);
+        const key = 'textWindow';
+        Object.keys(this.callBacks).forEach((callback)=>
+        {
 
-            ContainerMessage.addChild(spriteTextArea);
+            if(callback.startsWith(key) === true){
+                spriteWindow.on(this.callBacks[callback]["event"],(event)=>{this.callBacks[callback]["hook"](event,this,spriteWindow,textureWindow)});
+        
+                this._spriteDict[key] = {'spriteWindow':spriteWindow,"windowObject":textureWindow,"container":ContainerWindow,'methadata':{"alter_state":(header,message)=>textureWindow.addNewMessage(header,message),"type":"textWindow"}};
 
+                console.log(`${key} and alter_state: mapped with ${this.callBacks[callback]["hook"]} \n ${callback}`);
+            }
+            // callback.startsWith(key) === true? spriteTemporal.on(this.callBacks[callback]["event"],(event)=>{this.callBacks[callback]["hook"](event)}):false;
+          });
+        this.app.stage.addChild(spriteWindow);
 
-        this._spriteDict[key] = {'spriteWindows':spriteBack,'methadata':{'text':"hello world","alter_state":()=>this.callBacks[callback]["hook"](this,spriteBack,LoadSprite)},"type":"text"};
-        }
     }
     /*
     createObjectsDict(){
@@ -256,8 +405,14 @@ class MAIN_LEVEL extends UI_GAME{
         "ArrowRightOnClick":{"event":"click","hook":this.ArrowRightOnClick},
         "sound_OffOnClick":{"event":"click","hook":this.sound_OffOnClick},
         "sound_OnOnClick":{"event":"click","hook":this.sound_OnOnClick},
-        "love_levelUpgradeProgress":{"hook":this.love_levelUpgradeProgress}
+        "love_levelUpgradeProgress":{"hook":this.love_levelUpgradeProgress},
+        "textWindow":{"event":"wheel","hook":this.add_message}
+        
     }
+    }
+    add_message(event,ths,spriteWindow,textureWindow){
+        spriteWindow.addNewMessage("HUMAN","SECOND HELLO");
+
     }
     ArrowRightOnClick(event,ths,ButtonSettings,key,sprite){
         sprite.texture = ths.assets[ButtonSettings.assetHover];
@@ -290,6 +445,8 @@ class MAIN_LEVEL extends UI_GAME{
     async seg(){
         await super.seg();
         await this.rana();
+        console.log(this._spriteDict['textWindow'].windowObject._messageHistory)
+        this._spriteDict['textWindow'].methadata.alter_state('hello',' world')
         //this._spriteDict.love_level.methadata.alter_state();
 
     }
